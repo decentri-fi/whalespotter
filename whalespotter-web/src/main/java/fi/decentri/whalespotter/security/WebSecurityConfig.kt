adcdabdd3ef4fadc09f1d6c1ac94dbe79ffe84f5
Qkyrie <quintendeswaef@proton.me>
2023-01-30 15:55:44 +0100

    allow cors

--- whalespotter-web/src/main/java/fi/decentri/whalespotter/security/WebSecurityConfig.kt
+++ whalespotter-web/src/main/java/fi/decentri/whalespotter/security/WebSecurityConfig.kt
@@ -2,6 +2,7 @@ package fi.decentri.whalespotter.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
@@ -15,6 +16,7 @@ class WebSecurityConfig {
             .addFilterBefore(Web3SecurityFilter(), BasicAuthenticationFilter::class.java)
             .authorizeHttpRequests {
                 it
+                    .requestMatchers(HttpMethod.OPTIONS).permitAll()
                     .requestMatchers("/actuator/**").permitAll()
                     .anyRequest().authenticated()
 
