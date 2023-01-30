1dc1b9fb0fccae90de9e885496a0ea771df1ced7
Qkyrie <quintendeswaef@proton.me>
2023-01-30 13:01:10 +0100

    fix ci

--- ci/package.sh
+++ ci/package.sh
@@ -1,2 +1,2 @@
 docker build -t defitrack/whalespotter-runner:${BRANCH_NAME} whalespotter-runner -f ci/Dockerfile
-docker build -t defitrack/whalespotter-web-${BRANCH_NAME} whalespotter-web -f ci/Dockerfile
+docker build -t defitrack/whalespotter-web:${BRANCH_NAME} whalespotter-web -f ci/Dockerfile
