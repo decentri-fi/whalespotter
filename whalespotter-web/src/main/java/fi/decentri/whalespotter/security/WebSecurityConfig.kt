2f27a0dfe33c0510eb655463df8fef1da2bc07eb
Qkyrie <quintendeswaef@proton.me>
2023-01-30 16:03:41 +0100

    add cors conrig

--- whalespotter-web/src/main/java/fi/decentri/whalespotter/security/WebSecurityConfig.kt
+++ whalespotter-web/src/main/java/fi/decentri/whalespotter/security/WebSecurityConfig.kt
@@ -6,6 +6,9 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class WebSecurityConfig {
@@ -22,8 +25,17 @@ class WebSecurityConfig {
 
             }
             .csrf().disable()
-            .cors().disable()
         return http.build()
+    }
+
+    @Bean
+    fun provideCorsConfig(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("https://whalespotter.decentri.fi", "http://localhost:3000")
+        configuration.allowedMethods = listOf("GET", "POST", "HEAD", "OPTIONS", "PUT")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 
 }
