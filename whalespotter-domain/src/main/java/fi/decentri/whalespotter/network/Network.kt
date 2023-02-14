a38fdf37ec84962e2c5ab26098ec7dd4d63a4cc7
Qkyrie <quintendeswaef@proton.me>
2023-02-14 14:16:10 +0100

    add more info to networks

--- whalespotter-domain/src/main/java/fi/decentri/whalespotter/network/Network.kt
+++ whalespotter-domain/src/main/java/fi/decentri/whalespotter/network/Network.kt
@@ -1,11 +1,12 @@
 package fi.decentri.whalespotter.network
 
-enum class Network(val slug: String) {
-    ETHEREUM("ethereum"),
-    OPTIMISM("optimism"),
-    ARBITRUM("arbitrum"),
-    FANTOM("fantom"),
-    AVALANCHE("avalanche"),
-    BINANCE("binance"),
-    POLYGON("polygon")
+enum class Network(val slug: String, val logo: String) {
+    ETHEREUM("ethereum", "ethereum.png"),
+    OPTIMISM("optimism", "optimism.png"),
+    ARBITRUM("arbitrum", "arbitrum.png"),
+    FANTOM("fantom", "fantom.png"),
+    AVALANCHE("avalanche", "avalanche.png"),
+    BINANCE("binance", "bsc.svg"),
+    POLYGON("polygon", "polygon.png"),
+    STARKET("starknet.png", "starknet")
 }
