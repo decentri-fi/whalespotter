6748329c9b9c821f6456513d638fcc38b13d8b28
Qkyrie <quintendeswaef@gmail.com>
2023-02-24 10:24:52 +0100

    every hour update the things

--- whalespotter-runner/src/main/java/fi/decentri/transaction/cron/TransactionImportCron.kt
+++ whalespotter-runner/src/main/java/fi/decentri/transaction/cron/TransactionImportCron.kt
@@ -2,6 +2,7 @@ package fi.decentri.transaction.cron
 
 import fi.decentri.transaction.importer.TransactionImporter
 import fi.decentri.whalespotter.fish.repo.FishRepository
+import fi.decentri.whalespotter.whale.WhaleRepository
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
@@ -12,16 +13,22 @@ import kotlin.time.measureTime
 @Configuration
 class TransactionImportCron(
     private val transactionImporter: TransactionImporter,
-    private val fishRepository: FishRepository
+    private val fishRepository: FishRepository,
+    private val whaleRepository: WhaleRepository,
 ) {
 
     val logger = LoggerFactory.getLogger(this::class.java)
 
     @OptIn(ExperimentalTime::class)
-    @Scheduled(fixedDelay = 1000 * 60 * 10)
+    @Scheduled(fixedDelay = 1000 * 60 * 60) //every hour
     fun importTransactions() = runBlocking {
         val passed = measureTime {
-            fishRepository.findAll().forEach {
+            fishRepository.findAll().distinctBy {
+                it.address.lowercase()
+            }.forEach {
+                transactionImporter.import(it.address)
+            }
+            whaleRepository.findAll().forEach {
                 transactionImporter.import(it.address)
             }
         }
